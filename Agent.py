7736e6cd5a7fa434cf041babccf652cf717dda63
Masoud <m.shokrnezhad@gmail.com>
2024-05-16 15:12:41 +0300

    the agent has been updated to play based on the epsilon soft action selection strategy. the results are finally stands around the success rate of 0.4.

--- Agent.py
+++ Agent.py
@@ -2,51 +2,72 @@ import numpy as np
 
 
 class Agent():
-    def __init__(self, gamma=0.99):
+    def __init__(self, eps=0.1, gamma=0.99):
         self.my_hand = [i for i in range(3, 22)]
         self.dealer_hand = [i for i in range(1, 11)]
         self.ace_as_10 = [False, True]
         self.action_set = [0, 1]
-        self.value = {}
-        self.state = []
-        self.actual_values = {}
-        # self.is_state_visited = {}
+        
+        self.state_set = []
         self.memory = []
+        self.values = {} #mean values of states
+        self.values_ = {} #momentary values of states
+        self.policy = {}
+        
         self.gamma = gamma
+        self.eps = eps
 
         self.initialize()
+
 
     def initialize(self):
         for sum in self.my_hand:
             for num in self.dealer_hand:
                 for ace in self.ace_as_10:
-                    self.value[(sum, num, ace)] = 0
-                    self.actual_values[(sum, num, ace)] = []
-                    # self.is_state_visited[(sum, num, ace)] = 0
-                    self.state.append((sum, num, ace))
+                    state = (sum, num, ace)
+                    self.state_set.append(state)
+                    for action in self.action_set: 
+                        self.values[(state, action)] = 0
+                        self.values_[(state, action)] = []
+        
+        num_actions = len(self.action_set)
+        for state in self.state_set:
+            self.policy[state] = [1/num_actions for _ in range(num_actions)]
+
 
     def act(self, state):
-        (sum, _, _) = state
-        action = 0 if sum >= 21 else 1
+        action = np.random.choice(self.action_set, p=self.policy[state])
         return action
+
     
     def store(self, record):
         self.memory.append(record)
 
+
     def update(self):
-        for index, (state, _) in enumerate(self.memory):
+        #updating values_
+        for index, (state, action, _) in enumerate(self.memory):
             value_ = 0
-            # if self.is_state_visited[state] == 0:
-            # self.is_state_visited[state] += 1
             discount = 1
-            for _, (_, reward) in enumerate(self.memory[index:]):
+            for _, (_, _, reward) in enumerate(self.memory[index:]):
                 value_ += reward * discount
-                self.actual_values[state].append(value_)
+                discount *= self.gamma
+                self.values_[(state, action)].append(value_)
         
-        for state, _ in self.memory:
-            self.value[state] = np.mean(self.actual_values[state])
+        for state, action, _ in self.memory:
+            #updating values
+            self.values[(state, action)] = np.mean(self.values_[(state, action)])
             
-        # for state in self.state:
-        #     self.is_state_visited[state] = 0
+            #updating policies
+            max_action = np.argmax([self.values[(state, action_)] for action_ in self.action_set])
+            num_actions = len(self.action_set)
+            chances = []
+            for action_ in self.action_set:
+                if action_ == max_action:
+                    chance = 1 - self.eps + (self.eps / num_actions)
+                else:
+                    chance = self.eps / num_actions
+                chances.append(chance)
+            self.policy[state] = chances
             
         self.memory = []
--- main.py
+++ main.py
@@ -1,23 +1,29 @@
 import gym
+import matplotlib.pyplot as plt
 from Agent import Agent
 from tqdm import tqdm
-import collections.abc as cabc
 
 if __name__ == "__main__":
     env = gym.make("Blackjack-v1")
-    agent = Agent()
-    num_games = 100000
+    agent = Agent(eps=0.001)
+    num_games = 200000
+    results = {-1:0, 0:0, 1:0}
+    win_rates = []
+    
     for g in tqdm(range(num_games)):
-        raw_state = env.reset()
-        state = raw_state[0] #if isinstance(raw_state, cabc.Sequence) else raw_state
+        if g > 0 and g % 1000 == 0:
+            win_rates.append(results[1] / g)
+            
+        state = env.reset()[0]
         done = False
         while not done:
             action = agent.act(state)
             state_, reward, done, info, _ = env.step(action)
-            agent.store((state, reward))
+            agent.store((state, action, reward))
             state = state_
         agent.update()
-        
-    print(agent.value[(21, 3, True)])
-    print(agent.value[(4, 1, False)]) 
+        results[reward] += 1
+    
+    plt.plot(win_rates)
+    plt.show()
         
